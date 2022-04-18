4bbddb13885ef8f6965a0a839a13ee602c3e65b7
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-04-18 19:29:18 +0600

    authentication added

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const Register = () => {
-    const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
     const handleSubmit = event => {
-        event.preventDefault()
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
     }
 
     const navigateLogin = () =>{
         navigate('/login')
     }
+    if(user){
+        navigate('/home')
+    }
     return (
         <div className='register-form'>
             <h2 className='text-primary'>Please Register</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="" id="" placeholder='Your Name' />
-                <input type="email" name="" id="" placeholder='Your Email' required />
-                <input type="password" name="" id="" placeholder='Your Password' required />
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
                 <input type="submit" value="Register" />
             </form>
             <p>Already have an account? <Link to='/login' className='pe-auto text-primary' onClick={navigateLogin}>Please Login</Link></p>
